b244ead65260a8346d441490408a77d4dbcb8dcb
Mavis Atayo Adombire <145336116+madombire@users.noreply.github.com>
2024-12-06 01:38:53 +0100

    Worked on the forecast display

--- src/index.js
+++ src/index.js
@@ -61,6 +61,27 @@ function handleSearchSubmit(event) {
 // Attach event listener to form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-// Initial call to display weather for "Accra"
+function displayForecast() {
+  let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 searchCity("Accra");
+displayForecast();
